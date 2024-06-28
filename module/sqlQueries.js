4eaa2d10dc07b60c1265725b535ee34b65123b79
Manuel Smith <riddicksmith37@gmail.com>
2024-06-28 13:01:54 +0300

    changed fetch url and background

--- module/sqlQueries.js
+++ module/sqlQueries.js
@@ -1,5 +1,6 @@
 const insertUser = 'INSERT INTO users(username, emailaddress) VALUES($1, $2)'
 // const getUser = 'SELECT username, emailaddress FROM users WHERE emailaddress=$1'
 const getUser = `SELECT username, emailaddress FROM users WHERE emailaddress=$1`
+const getAllUsers = `SELECT * FROM users;`
 
-module.exports = { insertUser, getUser }
+module.exports = { insertUser, getUser, getAllUsers }
